6c5b8b973498f3bc4f3f3410fc4f44e2846b0adb
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-13 13:44:04 -0300

    fix: correção de responsividade

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,13 +2,13 @@ import { Button } from "../ui/button"
 
 export function Hero() {
   return (
-    <section className="wrapper bg-accent h-[calc(80dvh-5rem)] px-18 py-12 2xl:px-36 2xl:py-24">
-      <div className="flex h-full w-full flex-wrap items-center gap-12 xl:gap-24">
-        <div className="min-w-44 flex-1">
-          <h2 className="text-neutral-gray headline-2 2xl:headline-1">
+    <section className="wrapper bg-accent px-4 py-4 sm:px-9 sm:py-6 lg:px-18 lg:py-12 2xl:px-36 2xl:py-24">
+      <div className="grid items-center gap-8 md:grid-cols-2">
+        <div className="">
+          <h2 className="text-neutral-gray headline-3 sm:headline-2 2xl:headline-1">
             Lessons and insights
           </h2>
-          <h2 className="text-primary headline-2 2xl:headline-1">
+          <h2 className="text-primary headline-3 sm:headline-2 2xl:headline-1">
             from 8 years
           </h2>
           <p className="text-neutral-gray text-base/loose">
@@ -23,7 +23,7 @@ export function Hero() {
         <img
           src="/hero_illustration.svg"
           alt="Illustration"
-          className="h-full object-center"
+          className="justify-self-center object-center md:justify-self-end"
         />
       </div>
     </section>
